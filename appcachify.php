be00b4c3f3ef035996a4d08baf5f65881cd75437
Robert O'Rourke <robert@o-rourke.org>
2014-05-07 17:10:25 +0100

    Added plugin file

---
+++ appcachify.php
@@ -0,0 +1,323 @@
+<?php
+/*
+Plugin Name: Appcachify
+Plugin URI: http://interconnectit.com
+Description: Creates an appcache manifest for your static theme files. If your theme has a 307.php temaplte it is used when visitors are offline.
+Version: 0.1
+Author: Robert O'Rourke
+Author URI: http://interconnectit.com
+*/
+
+if ( ! class_exists( 'appcachify' ) ) {
+
+	add_action( 'plugins_loaded', array( 'appcachify', 'instance' ) );
+
+	class appcachify {
+
+		public $theme;
+
+		public $offline_file = '307.php';
+		public $offline_mode = false;
+
+		public $extensions;
+
+		public $output_buffer;
+
+		/**
+		 * @var object self
+		 */
+		protected static $instance = null;
+
+		/**
+		* Method to either return or create and return an instance of this
+		* class.
+		*
+		* @return appcachify
+		*/
+		public static function instance( ) {
+			null === self::$instance && self::$instance = new self;
+			return self::$instance;
+		}
+
+		public function __construct() {
+
+			if ( ! is_admin() )
+				add_action( 'wp_footer', array( $this, 'manifest_page_frame' ) );
+
+			add_action( 'template_redirect', array( $this, 'template_redirect' ) );
+
+			$this->theme = wp_get_theme();
+
+			$this->extensions = apply_filters( 'appcachify_extensions', array( 'jpg', 'jpeg', 'png', 'gif', 'svg', 'xml', 'swf' ) );
+
+			if ( file_exists( $this->theme->get_stylesheet_directory() . '/' . $this->offline_file ) )
+				$this->offline_mode = true;
+
+		}
+
+		public function request() {
+			global $wp, $post;
+
+			// prevent output to browser
+			$this->output_buffer = ob_get_clean();
+
+			if ( $wp->request == "manifest" ) {
+
+
+				header( 'HTTP/1.0 200 Ok' );
+				header( 'Content-type: text/html' );
+				header( 'Cache-Control: no-cache, must-revalidate' );
+
+				$this->manifest_page();
+				die;
+			}
+
+			if ( $wp->request == "manifest.appcache" ) {
+
+				header( 'HTTP/1.0 200 Ok' );
+				header( 'Content-type: text/cache-manifest' );
+				header( 'Cache-Control: max-age=3600, must-revalidate' );
+
+				$this->manifest();
+				die;
+			}
+
+		}
+
+		public function template_redirect( $template ) {
+			global $wp, $post;
+
+			if ( is_404() && $wp->request == "offline" && $this->offline_mode ) {
+
+				header( 'HTTP/1.0 307 Temporary Redirect' );
+				header( 'Cache-Control: max-age=3600, must-revalidate' );
+
+				return get_query_template( '307' );
+			}
+
+			if ( is_404() && in_array( $wp->request, array( 'manifest', 'manifest.appcache' ) ) ) {
+
+				// start output buffer so we capture queued scripts
+				$this->output_buffer = ob_start();
+
+				// add late scripts hook to add registered scripts to appcache
+				add_action( 'wp_enqueue_scripts', array( $this, 'request' ), 783921321 );
+
+			}
+
+			return $template;
+		}
+
+		public function manifest_url( $appcache = false, $echo = true ) {
+			$url = get_home_url() . '/manifest' . ( $appcache ? '.appcache' : '' );
+			if ( $echo )
+				echo $url;
+			return $url;
+		}
+
+		public function manifest_page() {
+			echo '<!DOCTYPE html><html manifest="' . $this->manifest_url( true, false ) . '"><head><title></title></head><body></body></html>';
+		}
+
+		public function manifest_page_frame() {
+			echo '<iframe style="display:none;" src="' . $this->manifest_url( false, false ) . '"></iframe>';
+		}
+
+		/**
+		 * Get the absolute filesystem path to the root of the WordPress installation
+		 *
+		 * @since 1.5.0
+		 *
+		 * @uses get_option
+		 * @return string Full filesystem path to the root of the WordPress installation
+		 */
+		function get_home_path() {
+			$home = get_option( 'home' );
+			$siteurl = get_option( 'siteurl' );
+			if ( ! empty( $home ) && 0 !== strcasecmp( $home, $siteurl ) ) {
+				$wp_path_rel_to_home = str_ireplace( $home, '', $siteurl ); /* $siteurl - $home */
+				$pos = strripos( str_replace( '\\', '/', $_SERVER['SCRIPT_FILENAME'] ), trailingslashit( $wp_path_rel_to_home ) );
+				$home_path = substr( $_SERVER['SCRIPT_FILENAME'], 0, $pos );
+				$home_path = trailingslashit( $home_path );
+			} else {
+				$home_path = ABSPATH;
+			}
+
+			return str_replace( '\\', '/', $home_path );
+		}
+
+		public function get_path_from_url( $url ) {
+
+			// is it a local file
+			if ( strstr( $url, get_home_url() ) ) {
+
+				// content url/dir replacement
+				$file = str_replace( WP_CONTENT_URL, WP_CONTENT_DIR, $url );
+
+				if ( file_exists( $file ) )
+					return $file;
+
+			}
+
+			// is it from includes
+			if ( strstr( $url, '/wp-includes/' ) ) {
+
+				$file = str_replace( site_url(), $this->get_home_path(), $url );
+
+				if ( file_exists( $file ) )
+					return $file;
+
+			}
+
+			return false;
+		}
+
+		public function get_assets( WP_Dependencies $assets ) {
+			$output = array();
+			foreach( $assets->queue as $handle ) {
+				$output = array_merge( $this->recurse_deps( $assets, $handle ), $output );
+			}
+			return array_filter( array_unique( $output ) );
+		}
+
+		public function recurse_deps( WP_Dependencies $assets, $handle ) {
+			$output = array();
+			$output[ $handle ] = preg_replace( '|^/wp-includes/|', includes_url(), $assets->registered[ $handle ]->src );
+			foreach( $assets->registered[ $handle ]->deps as $dep ) {
+				$output = array_merge( $this->recurse_deps( $assets, $dep ), $output );
+			}
+			return array_unique( $output );
+		}
+
+		public function manifest() {
+			global $wp_scripts, $wp_styles;
+
+			$cache = array();
+			$network = array();
+			$fallback = array();
+
+			// flag for when to refresh appcached scripts
+			$assets_updated = 0;
+			$assets_size = 0;
+
+			// get queued js & css
+			$cache += $this->get_assets( $wp_scripts );
+			$cache += $this->get_assets( $wp_styles );
+
+			if ( $this->offline_mode ) {
+				$network = array( '*' );
+				$fallback = array( '/ /offline/' );
+			}
+
+			$src_dir = $this->theme->get_stylesheet_directory();
+			$src_url = $this->theme->get_stylesheet_directory_uri();
+
+			// $assets = $this->process_dir( $src_dir, true );
+			$assets = $this->theme->get_files( $this->extensions, 10, false );
+
+			array_walk( $assets, function( &$item, $relative_path ) use ( $src_url, $src_dir ) {
+				if ( preg_match( '/screenshot\.(gif|png|jpg|jpeg|bmp)/', $relative_path ) )
+					$item = false;
+				else
+					$item = rtrim( $src_url, '/' ) . '/' . ltrim( $relative_path, '/' );
+				} );
+
+			$cache += $assets;
+
+			foreach( $cache as $url ) {
+				$filename = $this->get_path_from_url( $url );
+				if ( $filename ) {
+					$filemtime = filemtime( $filename );
+					$assets_updated = $assets_updated < $filemtime ? $filemtime : $assets_updated;
+					$assets_size += filesize( $filename );
+				}
+			}
+
+			foreach( array( 'cache', 'network', 'fallback' ) as $section ) {
+				$$section = array_filter( array_unique( apply_filters( "appcache_{$section}", $$section ) ) );
+				$$section = implode( "\n", $$section );
+			}
+
+			// flag to alter when manifest should be refetched
+			$update = implode( "\n# ", array_filter( array(
+				'Theme: ' . $this->theme->get_stylesheet() . ' ' . $this->theme->display( 'version', false ),
+				'Modified: ' . date( "Y-m-d H:i:s", $assets_updated ),
+				'Size: ' . number_format( $assets_size/1000, 0 ) . 'kb'
+			) ) );
+
+			echo "CACHE MANIFEST
+# $update
+
+";
+if ( ! empty( $cache ) ) :
+echo "
+# Explicitly cached 'master entries'.
+CACHE:
+$cache
+
+";
+endif;
+if ( ! empty( $network ) ) :
+echo "
+# Resources that require the user to be online.
+NETWORK:
+$network
+
+";
+endif;
+if ( ! empty( $fallback ) ) :
+echo "
+# Fallback resources if user is offline
+FALLBACK:
+$fallback
+
+";
+endif;
+
+		}
+
+		/**
+		 * Scans a directory recursively and provides hooks to modify files, folders
+		 * or both
+		 *
+		 * @param string $dir       Directory path
+		 * @param bool $recursive Whether to traverse directories recursively
+		 *
+		 * @return array|bool    Array of files and directories or false if $dir not found
+		 */
+		public function process_dir( $dir, $recursive = false ) {
+			if ( is_dir( $dir ) ) {
+				for ( $list = array(), $handle = opendir( $dir ); ( false !== ( $file = readdir( $handle ) ) ); ) {
+					if ( ( $file != '.' && $file != '..' ) && ( file_exists( $path = $dir . '/' . $file ) ) ) {
+						if ( is_dir( $path ) && ( $recursive ) ) {
+							$list = array_merge( $list, $this->process_dir( $path, true ) );
+						} else {
+							$entry = array( 'filename' => $file, 'dirpath' => $dir );
+
+							$entry = apply_filters( 'process_dir_entry', $entry );
+
+							do if ( ! is_dir( $path ) ) {
+
+								$entry = apply_filters( 'process_dir_file', $entry );
+
+								break;
+							} else {
+
+								$entry = apply_filters( 'process_dir_directory', $entry );
+
+								break;
+							} while ( false );
+
+							$list[] = $entry;
+						}
+					}
+				}
+				closedir( $handle );
+				return $list;
+			} else
+				return false;
+		}
+
+	}
+
+}
